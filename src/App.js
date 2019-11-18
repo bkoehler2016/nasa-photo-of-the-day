b19da1b9a09e6172df14e4d91dde4d5a0244c5e0
Ben Koehler <bkoehler85282@gmail.com>
2019-11-18 13:56:18 -0700

    setup first commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      this will be fun
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
