824bb24c4a4a8c26e19768077f8f85384042ab59
Ben Koehler <bkoehler85282@gmail.com>
2019-11-18 21:41:30 -0700

    setup basic sekelton code

---
+++ src/components/CardDisplay.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "./card";
+
+export default function CardDisplay({ lmit }) {
+  const [cardInfo, setCardInfo] = useState({});
+}
+return <div></div>;
---
+++ src/components/card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = props => {
+  return (
+    <div>
+      <img src="url" alt="NASA Image of the Day" />
+      <h2>Title</h2>
+      <h3>Date</h3>
+      <p>explanation</p>
+    </div>
+  );
+};
+export default Card;
