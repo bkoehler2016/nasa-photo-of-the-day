9ae79523419f4559936e4c1c9fe559e1360419f2
Ben Koehler <bkoehler85282@gmail.com>
2019-11-20 20:19:55 -0700

    changed background color for card

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import { Card as Cardstyle } from "reactstrap";
 const Card = props => {
   return (
     <div key={props.id}>
-      <Cardstyle color="info" className="text-white">
+      <Cardstyle color="dark" className="text-white">
         <img alt="Random NASA pic of the day" src={props.url} />
         <h2>{props.title}</h2>
         <h3>{props.date}</h3>
